25226d2c89a57ff37c9516e0740dac47bb8c7d1d
814850963 <z123123123111@163.com>
2022-07-01 13:44:57 +0900

    viewset router add

--- django_pet_mall/viewsettest/urls.py
+++ django_pet_mall/viewsettest/urls.py
@@ -1,3 +1,4 @@
+from email.mime import base
 from django.contrib import admin
 from django.urls import path, include, re_path
 from. import views
@@ -5,7 +6,21 @@ from. import views
 urlpatterns = [
     path('students/', views.StudentAPIView.as_view()),
     path('students1/', views.StudentMixinView.as_view()),
-    re_path("^students3/(?P<pk>\d+)/$}", views.StudentInfoMixinView.as_view())
+    re_path("^students3/(?P<pk>\d+)/$}", views.StudentInfoMixinView.as_view()),
 
-
+    # 视图集 ViewSet
+    path("students5/", views.StudentViewSet.as_view({"get": "get_list", "post":"post"})),
+    re_path("^students5/(?P<pk>\d+)/$", views.StudentViewSet.as_view({"get": "get_student_info"})),
 ]
+
+# 自动生成路由信息
+from rest_framework.routers import SimpleRouter, DefaultRouter
+
+# 实例化路由类
+router = DefaultRouter()
+
+# 给路由注册视图集
+router.register("student9", views.StudentViewSet, basename="student9")
+
+# 把生成的路由列表和urlpatterns进行拼接组合
+urlpatterns += router.urls
--- django_pet_mall/viewsettest/views.py
+++ django_pet_mall/viewsettest/views.py
@@ -1,14 +1,18 @@
 from rest_framework.views import APIView
 from rest_framework.generics import GenericAPIView
+from rest_framework.generics import ListAPIView
+from rest_framework.viewsets import ViewSet
 
 from rest_framework.response import Response
 from rest_framework.mixins import *
 from rest_framework import status 
 from mallapi.models import Student
 from .serializers import StudentModelSerializer
+from rest_framework.decorators import action
 
 # Create your views here.
 class StudentAPIView(APIView):
+    @action(methods=["get"], detail=False, url_path="/user/login")
     def get(self, request):
         """获取所有学生信息"""
         # 1 从数据库中读取学生信息
@@ -46,3 +50,46 @@ class StudentInfoMixinView(GenericAPIView, RetrieveModelMixin, UpdateModelMixin,
 
     def delete(self, request, pk):
         return self.delete(request, pk)
+
+class StudentViewSet(ViewSet):
+    def get_list(self, request):
+        student_list = Student.objects.all()
+        serializer = StudentModelSerializer(instance=student_list, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer =  StudentModelSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def get_student_info(self, request, pk):
+        print(request)
+        try:
+            student = Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = StudentModelSerializer(instance=student)
+
+        return Response(serializer.data)
+
+    def update(self, request, pk):
+        try:
+            student = Student.objects.get(pk=pk)
+        except Student.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = StudentModelSerializer(instance=student, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response(serializer.data)
+    
+    def delete(self,request, pk):
+        try:
+            Student.objects.get(pk=pk).delete()
+        except Student.DoesNotExist:
+            ...
+        return Response(status=status.HTTP_204_NO_CONTENT)
